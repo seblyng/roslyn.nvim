d781dfcdd3ce38c07577d313f746c0b872a2eece
Luca Frangiamore <Luca.Frangiamore@worlwolterskluwer.com>
2024-10-01 16:41:51 +0200

    feat: supporto nvim-tree

---
+++ lua/roslyn/nvim-tree.lua
@@ -0,0 +1,28 @@
+local M = {}
+
+M.setup_event = function()
+	local tree = require("nvim-tree.events")
+	if not package.loaded["nvim-tree.events"] then
+		print("Package is not loaded.")
+	end
+	local events = require("nvim-tree.events").Event
+
+	tree.subscribe(events.FileCreated, function(data)
+		require("roslyn.csprojManager").add_element(data.fname)
+	end)
+
+	tree.subscribe(events.WillRemoveFile, function(data)
+		require("roslyn.csprojManager").remove_element(data.fname)
+	end)
+
+	tree.subscribe(events.NodeRenamed, function(data)
+		require("roslyn.csprojManager").remove_element(data.old_name)
+		require("roslyn.csprojManager").add_element(data.new_name)
+	end)
+
+	tree.subscribe(events.NodeRenamed, function(data)
+		require("roslyn.csprojManager").remove_element(data.old_name)
+		require("roslyn.csprojManager").add_element(data.new_name)
+	end)
+end
+return M
