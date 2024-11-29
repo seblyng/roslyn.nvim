e6274dde165f86b04e6d122d96fa4eec0ac28612
Sebastian Lyng Johansen <sebastian@lyngjohansen.com>
2024-11-29 16:22:30 +0100

    fix: implement razor handler suggesting users to install rzls.nvim

--- lua/roslyn/lsp.lua
+++ lua/roslyn/lsp.lua
@@ -53,6 +53,9 @@ function M.start(bufnr, root_dir, on_init)
 
             return vim.NIL
         end,
+        ["razor/provideDynamicFileInfo"] = function(_, _, _)
+            return vim.notify("Razor is not supported. Please use https://github.com/tris203/rzls.nvim")
+        end,
     }, config.handlers or {})
     config.on_init = function(client, initialize_result)
         if roslyn_config.config.on_init then
