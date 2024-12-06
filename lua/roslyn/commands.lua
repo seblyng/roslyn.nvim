5c402d1d63302066e10ec1e0620d9ec33ca8f037
Sebastian Lyng Johansen <sebastian@lyngjohansen.com>
2024-12-06 11:58:11 +0100

    fix: do not use methods for client yet...
    It doesn't work on 0.10 yet, but `.` should work on nightly 0.11 for now

--- lua/roslyn/commands.lua
+++ lua/roslyn/commands.lua
@@ -19,14 +19,18 @@ local subcommand_tbl = {
 
             local attached_buffers = vim.tbl_keys(client.attached_buffers)
 
-            client:stop()
+            -- TODO: Change this to `client:request` when minimal version is `0.11`
+            ---@diagnostic disable-next-line: missing-parameter
+            client.stop()
 
             local timer = vim.uv.new_timer()
             timer:start(
                 500,
                 100,
                 vim.schedule_wrap(function()
-                    if client:is_stopped() then
+                    -- TODO: Change this to `client:request` when minimal version is `0.11`
+                    ---@diagnostic disable-next-line: missing-parameter
+                    if client.is_stopped() then
                         for _, buffer in ipairs(attached_buffers) do
                             vim.api.nvim_exec_autocmds("FileType", { group = "Roslyn", buffer = buffer })
                         end
@@ -46,7 +50,9 @@ local subcommand_tbl = {
                 return
             end
 
-            client:stop(true)
+            -- TODO: Change this to `client:request` when minimal version is `0.11`
+            ---@diagnostic disable-next-line: missing-parameter, param-type-mismatch
+            client.stop(true)
         end,
     },
     target = {
--- lua/roslyn/lsp_commands.lua
+++ lua/roslyn/lsp_commands.lua
@@ -30,6 +30,7 @@ end
 local function handle_fix_all_code_action(client, data)
     local flavors = data.arguments[1].FixAllFlavors
     vim.ui.select(flavors, { prompt = "Pick a fix all scope:" }, function(flavor)
+        -- TODO: Change this to `client:request` when minimal version is `0.11`
         client.request("codeAction/resolveFixAll", {
             title = data.title,
             data = data.arguments[1],
@@ -71,9 +72,12 @@ function M.nested_code_action(client)
             if action.code_action.data.FixAllFlavors then
                 handle_fix_all_code_action(client, action.code_action.command)
             else
-                client:request("codeAction/resolve", {
+                -- TODO: Change this to `client:request` when minimal version is `0.11`
+                ---@diagnostic disable-next-line: param-type-mismatch
+                client.request("codeAction/resolve", {
                     title = action.code_action.title,
                     data = action.code_action.data,
+                    ---@diagnostic disable-next-line: param-type-mismatch
                 }, function(err, response)
                     if err then
                         vim.notify(err.message, vim.log.levels.ERROR, { title = "roslyn.nvim" })
