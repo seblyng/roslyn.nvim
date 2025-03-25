012324d3327044581e11f2efa4af852824a223f5
Kemal YILDIRIM <kemal.yildirim@racelogic.co.uk>
2025-03-25 23:22:35 +0000

    formatted

--- lua/roslyn/sln/api.lua
+++ lua/roslyn/sln/api.lua
@@ -5,19 +5,19 @@ local M = {}
 ---@param match function A function that takes a line from the file, and returns a project path if the line contains a reference to a project file.
 ---@return string[] paths Paths to the projects in the solution
 local function projects_core(file, solution, match)
-	local paths = {}
+ local paths = {}
 
-	for line in file:lines() do
-		local path = match(line, vim.fn.fnamemodify(solution, ":e"))
-		if path then
-			local dirname = vim.fs.dirname(solution)
-			local fullpath = vim.fs.joinpath(dirname, path)
-			local normalized = vim.fs.normalize(fullpath)
-			table.insert(paths, normalized)
-		end
-	end
+ for line in file:lines() do
+  local path = match(line, vim.fn.fnamemodify(solution, ":e"))
+  if path then
+   local dirname = vim.fs.dirname(solution)
+   local fullpath = vim.fs.joinpath(dirname, path)
+   local normalized = vim.fs.normalize(fullpath)
+   table.insert(paths, normalized)
+  end
+ end
 
-	return paths
+ return paths
 end
 
 --- Attempts to extract the project path from a line in a solution file
@@ -25,42 +25,42 @@ end
 ---@param type "slnx" | "sln"
 ---@return string? path The path to the project file
 local function sln_match(line, type)
-	if type == "sln" then
-		local id, name, path = line:match('Project%("{(.-)}"%).*= "(.-)", "(.-)", "{.-}"')
-		if id and name and path and path:match("%.csproj$") then
-			return path
-		end
-	elseif type == "slnx" then
-		local path = line:match('<Project Path="([^"]+)"')
-		if path and path:match("%.csproj$") then
-			return path
-		end
-	else
-		error("Unknown type " .. type)
-	end
+ if type == "sln" then
+  local id, name, path = line:match('Project%("{(.-)}"%).*= "(.-)", "(.-)", "{.-}"')
+  if id and name and path and path:match("%.csproj$") then
+   return path
+  end
+ elseif type == "slnx" then
+  local path = line:match('<Project Path="([^"]+)"')
+  if path and path:match("%.csproj$") then
+   return path
+  end
+ else
+  error("Unknown type " .. type)
+ end
 end
 
 --- Attempts to extract the project path from a line in a solution filter file
 ---@param line string
 ---@return string? path The path to the project file
 local function slnf_match(line)
-	return line:match('"(.*%.csproj)"')
+ return line:match('"(.*%.csproj)"')
 end
 
 ---@param target string Path to solution or solution filter file
 ---@return string[] Table of projects in given solution
 function M.projects(target)
-	local file = io.open(target, "r")
-	if not file then
-		return {}
-	end
+ local file = io.open(target, "r")
+ if not file then
+  return {}
+ end
 
-	local paths = (target:match("%.sln$") or target:match("%.slnx$")) and projects_core(file, target, sln_match)
-	or target:match("%.slnf$") and projects_core(file, target, slnf_match)
+ local paths = (target:match("%.sln$") or target:match("%.slnx$")) and projects_core(file, target, sln_match)
+ or target:match("%.slnf$") and projects_core(file, target, slnf_match)
 
-	file:close()
+ file:close()
 
-	return paths
+ return paths
 end
 
 ---Checks if a project is part of a solution or not
@@ -69,17 +69,17 @@ end
 ---@param project string Full path to the csproj file
 ---@return boolean
 function M.exists_in_solution(solution, project)
-	vim.notify(
-		"`exists_in_solution` has been renamed `exists_in_target` and may be removed in a future release",
-		vim.log.levels.WARN,
-		{ title = "roslyn.nvim" }
-	)
+ vim.notify(
+  "`exists_in_solution` has been renamed `exists_in_target` and may be removed in a future release",
+  vim.log.levels.WARN,
+  { title = "roslyn.nvim" }
+ )
 
-	local projects = M.projects(solution)
+ local projects = M.projects(solution)
 
-	return vim.iter(projects):find(function(it)
-		return it == project
-	end) ~= nil
+ return vim.iter(projects):find(function(it)
+  return it == project
+ end) ~= nil
 end
 
 ---Checks if a project is part of a solution/solution filter or not
@@ -87,11 +87,11 @@ end
 ---@param project string Full path to the project's csproj file
 ---@return boolean
 function M.exists_in_target(target, project)
-	local projects = M.projects(target)
+ local projects = M.projects(target)
 
-	return vim.iter(projects):find(function(it)
-		return it == project
-	end) ~= nil
+ return vim.iter(projects):find(function(it)
+  return it == project
+ end) ~= nil
 end
 
 return M
