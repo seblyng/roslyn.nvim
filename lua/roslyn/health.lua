2f017f3873c94eedaa766208b6778e91be84a706
Kemal Yildirim <kemal.yildirim@racelogic.co.uk>
2025-04-02 16:11:14 +0100

    setup info slightly improved

--- lua/roslyn/health.lua
+++ lua/roslyn/health.lua
@@ -12,6 +12,8 @@ local check_config = function()
 {
     "seblyng/roslyn.nvim",
     ft = "cs",
+    ---@module 'roslyn.config'
+    ---@type RoslynNvimConfig
     opts = {
         -- your configuration comes here; leave empty for default settings
     }
@@ -36,39 +38,22 @@ local nvim_data_path = vim.fs.normalize(vim.fn.stdpath "data" .. "/mason/package
             '--logLevel=Information',
             '--extensionLogDirectory=' .. vim.fs.normalize(vim.fs.dirname(vim.lsp.get_log_path())),
             '--razorSourceGenerator=' .. nvim_data_path .. '/roslyn/libexec/Microsoft.CodeAnalysis.Razor.Compiler.dll',
-            '--razorDesignTimePath=' .. nvim_data_path .. '/rzls/libexec/Targets/Microsoft.NET.Sdk.Razor.DesignTime.targets',
         },
-        filewatching = "auto",
-        broad_search = true,
-        lock_target = true,
-        debug_enabled = false,
         exe = {
             "dotnet",
             nvim_data_path .. "/roslyn/libexec/Microsoft.CodeAnalysis.LanguageServer.dll",
         },
 
-        ---@diagnostic disable-next-line: missing-fields            
+        ---@diagnostic disable-next-line: missing-fields
         config = {
             settings = {
                 ["csharp|inlay_hints"] = {
                     csharp_enable_inlay_hints_for_implicit_object_creation = true,
-                    csharp_enable_inlay_hints_for_implicit_variable_types = true,
-                    csharp_enable_inlay_hints_for_lambda_parameter_types = true,
-                    csharp_enable_inlay_hints_for_types = true,
-                    dotnet_enable_inlay_hints_for_indexer_parameters = true,
-                    dotnet_enable_inlay_hints_for_literal_parameters = true,
-                    dotnet_enable_inlay_hints_for_object_creation_parameters = true,
-                    dotnet_enable_inlay_hints_for_other_parameters = true,
-                    dotnet_enable_inlay_hints_for_parameters = true,
-                    dotnet_suppress_inlay_hints_for_parameters_that_differ_only_by_suffix = true,
-                    dotnet_suppress_inlay_hints_for_parameters_that_match_argument_name = true,
-                    dotnet_suppress_inlay_hints_for_parameters_that_match_method_intent = true,
                 },
                 ["csharp|code_lens"] = {
                     dotnet_enable_references_code_lens = true,
                 },
             },
-            handlers = require 'rzls.roslyn_handlers',
         },
     },
 }
