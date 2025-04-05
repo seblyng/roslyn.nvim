0a9a5d253c1d2a65b8b259a8e59a5745fa3b57b7
Sebastian Lyng Johansen <sebastian@lyngjohansen.com>
2025-04-05 13:29:30 +0200

    feat: add some simple logging

--- lua/roslyn/commands.lua
+++ lua/roslyn/commands.lua
@@ -11,6 +11,12 @@ local cmd_name = "Roslyn"
 
 ---@type RoslynSubcommandTable[]
 local subcommand_tbl = {
+    log = {
+        impl = function()
+            local log = require("roslyn.log")
+            vim.cmd(string.format("tabnew %s", log.__log_file_path))
+        end,
+    },
     restart = {
         impl = function()
             local client = vim.lsp.get_clients({ name = "roslyn" })[1]
