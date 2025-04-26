169665a8b71098602813c4177056dd6bac9ee28b
Sebastian Lyng Johansen <sebastian@lyngjohansen.com>
2025-04-26 16:26:39 +0200

    fix: change wording of initializing message

--- lua/roslyn/lsp/on_init.lua
+++ lua/roslyn/lsp/on_init.lua
@@ -2,14 +2,14 @@ local M = {}
 
 function M.sln(client, solution)
     vim.g.roslyn_nvim_selected_solution = solution
-    vim.notify("Initializing Roslyn client for " .. solution, vim.log.levels.INFO, { title = "roslyn.nvim" })
+    vim.notify("Initializing Roslyn for: " .. solution, vim.log.levels.INFO, { title = "roslyn.nvim" })
     client:notify("solution/open", {
         solution = vim.uri_from_fname(solution),
     })
 end
 
 function M.projects(client, projects)
-    vim.notify("Initializing Roslyn client for projects", vim.log.levels.INFO, { title = "roslyn.nvim" })
+    vim.notify("Initializing Roslyn for: projects", vim.log.levels.INFO, { title = "roslyn.nvim" })
     client:notify("project/open", {
         projects = vim.tbl_map(function(file)
             return vim.uri_from_fname(file)
