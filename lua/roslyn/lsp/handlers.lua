87d0f38eab1c64bac716e28556a134d4012811de
kirilchik27 <bringonthepretty@gmail.com>
2025-08-15 00:44:12 +0300

    fix formatting

--- lua/roslyn/lsp/handlers.lua
+++ lua/roslyn/lsp/handlers.lua
@@ -63,20 +63,20 @@ return {
         end
     end,
     ['workspace/_roslyn_projectNeedsRestore'] = function(_, result, ctx)
-	local client = assert(vim.lsp.get_client_by_id(ctx.client_id))
+        local client = assert(vim.lsp.get_client_by_id(ctx.client_id))
 
-	---@diagnostic disable-next-line: param-type-mismatch
-	client:request('workspace/_roslyn_restore', result, function(err, response)
-	  if err then
-	    vim.notify(err.message, vim.log.levels.ERROR, { title = 'roslyn.nvim' })
-	  end
-	  if response then
-	    for _, v in ipairs(response) do
-	      vim.notify(v.message, vim.log.levels.INFO, { title = 'roslyn.nvim' })
-	    end
-	  end
-	end)
+        ---@diagnostic disable-next-line: param-type-mismatch
+        client:request('workspace/_roslyn_restore', result, function(err, response)
+          if err then
+            vim.notify(err.message, vim.log.levels.ERROR, { title = 'roslyn.nvim' })
+          end
+          if response then
+            for _, v in ipairs(response) do
+              vim.notify(v.message, vim.log.levels.INFO, { title = 'roslyn.nvim' })
+            end
+          end
+        end)
 
-	return vim.NIL
+        return vim.NIL
     end,
 }
