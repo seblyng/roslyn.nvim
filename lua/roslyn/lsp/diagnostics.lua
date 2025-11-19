88f837a658df7304ae47fdf251c9560ffbc6bf2b
Sebastian Lyng Johansen <sebastian@lyngjohansen.com>
2025-11-19 08:30:04 +0100

    fix: remove usage of deprecated lsp method

--- lua/roslyn/lsp/diagnostics.lua
+++ lua/roslyn/lsp/diagnostics.lua
@@ -2,8 +2,7 @@ local M = {}
 
 ---@param client vim.lsp.Client
 function M.refresh(client)
-    local buffers = vim.lsp.get_buffers_by_client_id(client.id)
-    for _, buf in ipairs(buffers) do
+    for buf in pairs(client.attached_buffers) do
         if vim.api.nvim_buf_is_loaded(buf) then
             client:request(
                 vim.lsp.protocol.Methods.textDocument_diagnostic,
