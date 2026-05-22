3a535cb9b5af556e23dcaf25aa6262472b90e6b1
Sebastian Lyng Johansen <sebastian@lyngjohansen.com>
2026-05-22 23:16:00 +0200

    chore: make API surface of store smaller
    I felt like it was too many exposed methods from the store module.
    We can keep the API surface smaller

--- lua/roslyn/store.lua
+++ lua/roslyn/store.lua
@@ -4,19 +4,14 @@ local client_id_to_solution = {}
 
 ---@param client_id integer
 ---@param solution? string
-function M.set_client_target(client_id, solution)
+function M.set(client_id, solution)
     client_id_to_solution[client_id] = solution
     M.set_selected_target(solution)
 end
 
 ---@param client_id integer
-function M.clear_client_target(client_id)
-    M.set_client_target(client_id, nil)
-end
-
----@param client_id integer
 ---@return string?
-function M.get_client_target(client_id)
+function M.get(client_id)
     return client_id_to_solution[client_id]
 end
 
@@ -28,18 +23,6 @@ end
 ---@return string?
 function M.get_selected_target()
     return vim.g.roslyn_nvim_selected_solution
-end
-
----@param bufnr integer
-function M.sync_selected_target_for_buffer(bufnr)
-    if require("roslyn.config").get().lock_target then
-        return
-    end
-
-    local client = vim.lsp.get_clients({ name = "roslyn", bufnr = bufnr })[1]
-    if client then
-        M.set_selected_target(M.get_client_target(client.id))
-    end
 end
 
 return M
